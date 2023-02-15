283dc8dceaef7f1112d78f7e8e2b5667b80b9b07
sharapin <sharapin@pmfst.hr>
2023-02-15 11:03:41 +0100

    ispit

--- Ispit_1/Zadatak_1/Analyzer.cpp
+++ Ispit_1/Zadatak_1/Analyzer.cpp
@@ -23,7 +23,7 @@ void Analyzer::Fit(){
 	Long64_t nentries = tree -> GetEntriesFast();
 	canvas = new TCanvas();
 	canvas -> SetCanvasSize(700, 700);
-	hist = new TH1F("hist", "hist", 100, 0., 0.1);
+	hist = new TH1F("hist", "hist", 100, 0., 0.03);
 	for (int i = 0; i < nentries; i++){
 
 		tree -> GetEntry(i);
@@ -31,13 +31,13 @@ void Analyzer::Fit(){
 	
 	}
 	gStyle -> SetOptStat(0);
-	f = new TF1("fit funkcija", "(TMath::Exp(-x*[0]) * TMath::Sin(x * [1]))", 0, 0.1);
+	f = new TF1("fit funkcija", "(TMath::Exp(-x*[0]) * TMath::Sin(x * [1]))", 0, 0.03);
 	f -> SetParameter(0, 0.2);
 	f -> SetParameter(1, 2.);
 
-        hist -> SetMaximum(0.1);        
+        hist -> SetMaximum(0.15);        
 
-	hist -> Fit(f, "L", "", 0, 1);
+	hist -> Fit(f, "L", "", 0, 0.03);
 
 	hist -> SetTitle("Fit_Function");
 	hist -> GetXaxis() -> SetTitle("x");
